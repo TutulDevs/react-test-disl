9bcbf76004b8fd1a717d98871993b08fe801e1b5
Tutul <tutulnahid@gmail.com>
2021-09-20 01:43:42 +0600

    Form: restyled

--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -3,6 +3,15 @@ import { useHistory } from "react-router";
 import PageHeader from "../components/UI/PageHeader";
 import AppContext from "../store/AppContext";
 
+// classes
+const classes = {
+  form: `flex flex-col justify-center items-center p-4 pt-8 h-5/6`,
+  formControl: `w-full sm:w-72 mt-3`,
+  formLabel: `block mb-1 ml-1`,
+  formInput: `w-full py-2 px-4 rounded-md outline-none ring-1 focus:ring-4 ring-green-300 bg-white duration-300`,
+  button: `w-full py-2 pt-2.5 px-6 bg-green-600 text-white text-lg font-semibold  tracking-wider rounded-full shadow-lg hover:shadow-none`,
+};
+
 const Form = () => {
   // input refs
   const nameRef = useRef();
@@ -53,13 +62,10 @@ const Form = () => {
       <PageHeader title="Form" />
 
       {/* Form components */}
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col justify-center items-center p-4 h-5/6"
-      >
+      <form onSubmit={handleSubmit} className={classes.form}>
         {/* Name formControl */}
-        <div className="w-60 mt-3 formControl">
-          <label htmlFor="name" className="block mb-1 ml-1">
+        <div className={classes.formControl}>
+          <label htmlFor="name" className={classes.formLabel}>
             Full Name
           </label>
           <input
@@ -69,13 +75,13 @@ const Form = () => {
             id="name"
             required
             placeholder="John Doe"
-            className="rounded-md ring-1 ring-green-300 py-2 px-4"
+            className={classes.formInput}
           />
         </div>
 
         {/* Email formControl */}
-        <div className="w-60 mt-3 formControl">
-          <label htmlFor="email" className="block mb-1 ml-1">
+        <div className={classes.formControl}>
+          <label htmlFor="email" className={classes.formLabel}>
             Email
           </label>
           <input
@@ -85,13 +91,13 @@ const Form = () => {
             id="email"
             required
             placeholder="john@doe.com"
-            className="rounded-md ring-1 ring-green-300 py-2 px-4"
+            className={classes.formInput}
           />
         </div>
 
         {/* Gender formControl */}
-        <div className="w-60 mt-3 formControl">
-          <label htmlFor="gender" className="block mb-1 ml-1">
+        <div className={classes.formControl}>
+          <label htmlFor="gender" className={classes.formLabel}>
             Gender
           </label>
           <select
@@ -99,7 +105,7 @@ const Form = () => {
             name="gender"
             id="gender"
             required
-            className="w-60 bg-white rounded-md ring-1 ring-green-300 py-2 px-4 appearance-none"
+            className={`${classes.formInput} appearance-none`}
           >
             <option value="male">Male</option>
             <option value="female">Female</option>
@@ -107,8 +113,8 @@ const Form = () => {
         </div>
 
         {/* Phone formControl */}
-        <div className="w-60 mt-3 formControl">
-          <label htmlFor="phone" className="block mb-1 ml-1">
+        <div className={classes.formControl}>
+          <label htmlFor="phone" className={classes.formLabel}>
             Telephone
           </label>
           <input
@@ -118,16 +124,13 @@ const Form = () => {
             id="phone"
             required
             placeholder="015...."
-            className="rounded-md ring-1 ring-green-300 py-2 px-4 appearance-none"
+            className={`${classes.formInput} appearance-none`}
           />
         </div>
 
         {/* Button formControl */}
-        <div className="w-60 mt-6 formControl text-center">
-          <button
-            type="submit"
-            className="w-full tracking-wider py-2 pt-3 px-6 bg-green-600 text-white text-lg font-semibold  rounded-full shadow-lg hover:shadow-none"
-          >
+        <div className={`${classes.formControl} mt-6 text-center`}>
+          <button type="submit" className={classes.button}>
             SUBMIT
           </button>
         </div>
